e8b25fa67494ceb191fc3325611ebd7a1d63663f
Azmitia <hamex12@gmail.com>
2019-10-30 12:17:15 -0600

    V1- se añadieron condicionales

--- main.cpp
+++ main.cpp
@@ -3,5 +3,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 int main(int argc, char** argv) {
+	int edad;
+	cout << "Indique la edad del usuario" << endl;
+	cin >> edad;
+	if(edad < 6){
+		
+	}else if(edad > 6 && edad <= 12){
+		
+	}else if(edad < 12 && edad <= 18){
+		
+	}else if(edad > 18 && edad <= 65){
+		
+	}else{
+		
+	}
 	return 0;
 }
